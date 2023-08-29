7545eaa1dbdfa36e576139f084bbc5dfa8425b21
Matthew Lemon <y@yulqen.org>
2023-08-29 19:29:08 +0100

    Basic implementation of collection of filenames
    This is the first basic implementation of main calling a function that
    gathers the filenames of the target files. This does not use the struct
    that we want to use yet, but I learned a lot about dynamic memory here.
    I even used valgrind to fix memory leaks from not freeing stuff
    afterwards. This version has no memory leaks.

--- dir.c
+++ dir.c
@@ -9,7 +9,7 @@
  * https://stackoverflow.com/questions/1442116/how-to-get-the-date-and-time-values-in-a-c-program
  */
 
-char **get_relevant_files()
+char **get_relevant_files(int *linesize)
 {
     DIR *journal_dir;
     struct dirent *dir_information;
@@ -22,8 +22,8 @@ char **get_relevant_files()
         perror("Error opening directory.");
     }
 
-    printf("Our markdown files: \n\n");
-
+    // First we have to iterate through the dir to count the entries
+    // we want....
     while ((dir_information = readdir(journal_dir)) != 0)
     {
         strcpy(fullpath, JOURNAL_DIR_PATH);
@@ -37,39 +37,49 @@ char **get_relevant_files()
             }
         }
     }
+    // Exposing the linecount to the caller
+    *linesize = linecount;
+    // Make sure to close the directory stream
+    if ((closedir(journal_dir) != 0))
+    {
+        perror("Unable to close directory");
+    }
 
+    // Request enough memory for array
     char **string_array = malloc(linecount * sizeof(char *));
     for (int i = 0; i < linecount; ++i)
     {
+        // 256 is arbitary here; it means we can only store 256 chars
         string_array[i] = malloc(256 * sizeof(char));
     }
 
-    // TODO: Need to reopen this directory and go again
-    /* if ((journal_dir = opendir(JOURNAL_DIR_PATH)) == NULL) */
-    /* { */
-    /*     perror("Error opening directory."); */
-    /* } */
+    // Now we open the directory again and create another stream
+    if ((journal_dir = opendir(JOURNAL_DIR_PATH)) == NULL)
+    {
+        perror("Error opening directory.");
+    }
+    // A new struct for directory entries
     struct dirent *dir_information2;
 
+    int idx = 0;
     while ((dir_information2 = readdir(journal_dir)) != 0)
     {
         strcpy(fullpath, JOURNAL_DIR_PATH);
         strcat(fullpath, "/");
-        strcat(fullpath, dir_information->d_name);
+        strcat(fullpath, dir_information2->d_name);
         if (!stat(fullpath, &file_stat))
         {
             if (!S_ISDIR(file_stat.st_mode))
             {
-                int idx = 0;
                 int length = strlen(fullpath);
                 // get pointer three chars back from the end of the string
                 const char *m_ext = fullpath + length - 3;
                 const char *t_ext = fullpath + length - 4;
                 if (strcmp(m_ext, ".md") == 0 || strcmp(t_ext, ".txt") == 0)
                 {
-                    printf("File: %s", fullpath);
+                    /* printf("File: %s", fullpath); */
                     {
-                        printf(" |-> extension is %s\n", m_ext);
+                        /* printf(" |-> extension is %s\n", m_ext); */
                         strcpy(string_array[idx], fullpath);
                         idx++;
                     }
@@ -77,13 +87,24 @@ char **get_relevant_files()
             }
         }
     }
+    // Make sure to close the directory stream
+    if ((closedir(journal_dir) != 0))
+    {
+        perror("Unable to close directory");
+    }
     return string_array;
 }
 
 int main(int argc, char *argv[])
 {
-    char **toss = get_relevant_files();
+    int s;
+    char **toss = get_relevant_files(&s);
+    printf("There seemingly were %d lines.\n", s);
     printf("%s\n", toss[0]);
+    for (int i = 0; i < s; ++i)
+    {
+        free(toss[i]);
+    }
     free(toss);
     return 0;
 }
