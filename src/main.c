2dbaa0e4adf5dcceccf702ae51836d40b4c38771
Matthew Lemon <y@yulqen.org>
2023-09-03 16:06:44 +0100

    Removed comment - tgz version working - not plain md

--- src/main.c
+++ src/main.c
@@ -5,20 +5,14 @@
 
 int main(int argc, char *argv[])
 {
-    // TODO what needs doing:
-    //  - Get all the markdown and text files using GetRelevantFiles()
-    //  - Iterate through each file and seach for the search term
-    //  - Create a JournalLine object based on the matching line and the file path
-    //  - Add this object to an array.
-    //  - Return this to the caller.
-    //  - A list function takes this array and prints out the matching lines and file paths
-    //      - In doing so, it highlights the search term
-    //  - Everything is formatted nicely
     ParseArgs(argc, argv);
-    char **dirs = (char *[]){//        "/home/lemon/Documents/Notes/journal/home",
-                             //        "/home/lemon/Documents/Notes/MOD/work_journal",
-                             //        "/home/lemon/Documents/Notes/Archive",
-                             "/home/lemon/Documents/Notes/journal/archives"};
+    char **dirs = (char *[])
+    {
+        //        "/home/lemon/Documents/Notes/journal/home",
+        //        "/home/lemon/Documents/Notes/MOD/work_journal",
+        //        "/home/lemon/Documents/Notes/Archive",
+                "/home/lemon/Documents/Notes/journal/archives"
+    };
     int counter = 0;
     JournalLine **toss = putLinesFromRelevantFilesIntoJournalLines(&counter, dirs);
     for (int i = 0; i < counter; ++i)
