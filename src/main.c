9d9708287b13d04db174b809e2eaed6a5853a29d
Matthew Lemon <y@yulqen.org>
2023-09-05 16:54:26 +0100

    Basically now reached parity with C++ version
    This still produces errors on some search terms but functionality now largely matches C++ version, except no command line arguments yet - hard-coded search term.

--- src/main.c
+++ src/main.c
@@ -7,30 +7,32 @@
 int main(int argc, char *argv[])
 {
     ParseArgs(argc, argv);
-    printf("%s\n", highlight_search_term("longer is the night", "night"));
+    //    printf("%s\n", highlight_search_term("longer is the night", "night"));
 
-    //    char *dirs[] = {"/home/lemon/Documents/Notes/journal/home", "/home/lemon/Documents/Notes/MOD/work_journal",
-//                    "/home/lemon/Documents/Notes/Archive", "/home/lemon/Documents/Notes/journal/archives"};
-//    // Calculate the size of the array of dicts
-//    int dir_count = sizeof dirs / sizeof dirs[0];
-//
-//    int idx = 0;
-//    JournalLine **toss = journal_search_directories_search_term(&idx, dir_count, dirs, "Python");
-//    char *current_fn = NULL;
-//    for (int i = 0; i < idx; ++i)
-//    {
-//        if (current_fn == NULL || strcmp(current_fn, toss[i]->filename) != 0)
-//        {
-//            printf("\n%s\n", toss[i]->filename);
-//            current_fn = toss[i]->filename;
-//        }
-//        printf("%s\n", toss[i]->line);
-//    }
-//    for (int i = 0; i < idx; ++i) {
-//        free(toss[i]->line);
-//        free(toss[i]->filename);
-//        free(toss[i]);
-//    }
-////    free(toss);
+    char *dirs[] = {"/home/lemon/Documents/Notes/journal/home", "/home/lemon/Documents/Notes/MOD/work_journal",
+                    "/home/lemon/Documents/Notes/Archive", "/home/lemon/Documents/Notes/journal/archives"};
+    // Calculate the size of the array of dicts
+    int dir_count = sizeof dirs / sizeof dirs[0];
+
+    int idx = 0;
+    char *search_term = "zettle";
+    JournalLine **toss = journal_search_directories_search_term(&idx, dir_count, dirs, search_term);
+    char *current_fn = NULL;
+    for (int i = 0; i < idx; ++i)
+    {
+        if (current_fn == NULL || strcmp(current_fn, toss[i]->filename) != 0)
+        {
+            printf("\n%s\n", toss[i]->filename);
+            current_fn = toss[i]->filename;
+        }
+        printf("%s\n", highlight_search_term(toss[i]->line, search_term));
+    }
+    for (int i = 0; i < idx; ++i)
+    {
+        free(toss[i]->line);
+        free(toss[i]->filename);
+        free(toss[i]);
+    }
+    //    free(toss);
     return 0;
 }
